73de6da51cf5a73f4ceb4451b148a40fb46638d4
Shaun Singh <sdsingh@berkeley.edu>
2013-06-21 10:48:49 -0500

    Non-logged in user bug fix

--- static/functions.js
+++ static/functions.js
@@ -7,24 +7,35 @@
     //REPLACE WITH LINKS TO YOUR FAVORITES BUTTON IMAGES (src1 is not favorited, src2 is favorited)
     var imgSRC={'src1':'/static/images/not_favorited.png','src2':'/static/images/favorited.png'};
     //Controller for button of something that is not favorited
-    $('.favorites').live('click', function() {
-        var img=$(this);
-        //Note: id represents the id of the element you want to be favorited
-        //replace with the proper selector 
-        id=$(this).parent().parent().attr('id');
-        $.post('/favorite/', {"id": id}, function(data) {
-          //functionality for an unsuccessful request can easily be added by checking for data.length = 0
-          img.attr('src', imgSRC.src2);
-          img.attr('class', 'favorites_on');
-        });
-    });
-
-    $('.favorites_on').live('click', function() {
-        var img=$(this);
-        id=$(this).parent().parent().attr('id');
-        console.log(id);
-        $.post('/delete_favorite/', {"id": id}, function(data) {
-          img.attr('src', imgSRC.src1);
-          img.attr('class', 'favorites');
-        });
-	});
+      $('.favorites').live('click', function() {
+        if(USER_STATUS == 'true'){
+           //user is logged in, let's go!
+           var img=$(this);
+           //Note: id represents the id of the element you want to be favorited
+           //replace with the proper selector 
+           id=$(this).parent().parent().attr('id');
+           console.log(id);
+           $.post('/favorite/', {"id": id}, function(data) {
+               img.attr('src', imgSRC.src2);
+               img.attr('class', 'favorites_on');
+           });
+        } else {
+           //not logged in, redirect to login page!
+           window.location = '/login/';
+        }
+      });
+      $('.favorites_on').live('click', function() {
+        if(USER_STATUS == 'true'){
+           //user is logged in, let's go!
+           var img=$(this);
+           id=$(this).parent().parent().attr('id');
+           console.log(id);
+           $.post('/favorite/', {"id": id}, function(data) {
+               img.attr('src', imgSRC.src1);
+               img.attr('class', 'favorites');
+           });
+        } else {
+           //not logged in, redirect to login page!
+           window.location = '/login/';
+        }
+      });
